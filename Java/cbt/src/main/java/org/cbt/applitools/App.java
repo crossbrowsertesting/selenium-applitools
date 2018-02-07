782a1650e4384194dc26067fe44eabe5b439e13e
chasecook412 <chase@crossbrowsertesting.com>
2018-02-07 17:49:59 -0600

    committing project

---
+++ Java/cbt/src/main/java/org/cbt/applitools/App.java
@@ -0,0 +1,13 @@
+package org.cbt.applitools;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
